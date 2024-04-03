aaeeae9711e895a61887edbc3255f6ec754ba2c8
fuzionu <fuzionu@gmail.com>
2024-04-03 21:58:28 +0200

    add fileLoader in Python lang

---
+++ python/main.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+	if os.path.isfile("dupa.txt"):
+		print("Wszystko git, jest plik mordo")
+		
+	else:
+		print("Nie ma takiego pliku wariacie")
+	
+	
+	
+if __name__ == "__main__":
+	main()
