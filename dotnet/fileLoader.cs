d8fd77cb9905b6bae1e62e18fba720529ca99b16
fuzionu <fuzionu@gmail.com>
2024-04-04 22:46:52 +0200

    pass filename as shell arguments in C# lang

--- dotnet/fileLoader.cs
+++ dotnet/fileLoader.cs
@@ -5,11 +5,23 @@ class fileLoader
 {
 	static public void Main(String[] args)
 	{
-		if(File.Exists("dupa.txt"))
+		
+		if (args.Length == 0)
 		{
-			string text = File.ReadAllText("dupa.txt");
+			Console.WriteLine("Podaj nazwe pliku byku");
+		}
+		
+		else if (args.Length > 1)
+		{
+			Console.WriteLine("Podaj tylko jeden plik");
+		}
+		
+		else if (File.Exists(args[0]))
+		{
+			string text = File.ReadAllText(args[0]);
 			Console.WriteLine(text);
 		}
+		
 		else
 		{
 			Console.WriteLine("Nie ma takiego pliku wariacie");
