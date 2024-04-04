fea5051046d572fe30a375944aca8111adb734f8
fuzionu <fuzionu@gmail.com>
2024-04-04 21:12:58 +0200

    add printing txt file contents in Java lang

--- java/fileLoader.java
+++ java/fileLoader.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.io.IOException;
 
 public class fileLoader
@@ -6,14 +8,23 @@ public class fileLoader
 	public static void main(String[] args)
 	{
 		File file = new File("dupa.txt");
-			
-		if (file.isFile())
+		Path path = Path.of("dupa.txt");
+		
+		try
 		{
-			System.out.print("Wszystko git, plik jest mordo");
+			if (file.isFile())
+			{
+				String text = Files.readString(path);
+				System.out.print(text);
+			}
+			else
+			{
+				System.out.print("Nie ma takiego pliku wariacie");
+			}
 		}
-		else
+		catch (IOException e)
 		{
-			System.out.print("Nie ma takiego pliku wariacie");
+			System.out.print("Error");
 		}
 	}
 }
