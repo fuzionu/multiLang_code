dad2914d8ca3095ff89e752e741fa1f7df755b34
fuzionu <fuzionu@gmail.com>
2024-04-07 19:25:10 +0200

    pass filename as shell arguments in PHP lang

--- php/fileLoader.php
+++ php/fileLoader.php
@@ -1,6 +1,13 @@
 <?php
-	if (file_exists("dupa.txt"))
-		echo file_get_contents("dupa.txt");
+	if (!isset($argv[1]))
+		echo "Podaj nazwe pliku byku";
+	
+	else if (count($argv) > 2)
+		echo "Podaj tylko jeden plik";
+		
+	else if (file_exists($argv[1]))
+		echo file_get_contents($argv[1]);
+	
 	else
 		echo "Nie ma takiego pliku wariacie";
 ?>
